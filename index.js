c42ff3a1243be2bbe86041e836e4e82dbde528a7
Charlie Hengemihle <charliehengemihle@gmail.com>
2023-01-10 13:59:31 -0700

    shenanigans

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ export const getName = ({ name }) => name
 // OUTPUT: the string 'Bob Smith lives at 123 Main Street, Anytown, USA'
 // REQS: use destructuring and template literals
 
-export const printAddress = ({ name, address }) =>
-  `${name} lives at ${address.number} ${address.street}, ${address.city}, ${address.country}`
+export const printAddress = ({ name, address: { number, street, city, country } }) =>
+  `${name} lives at ${number} ${street}, ${city}, ${country}`
 
 // REFACTOR CHALLENGE
 // Refactor this function so that all values in the object are destructured
@@ -40,14 +40,22 @@ export const printUserInfo = ({
 // REQS: use rest parameters
 //  getSum(1, 2, 3) === 6
 //  getSum(1, 2, 3, 4, 5) === 15
-export const getSum = () => {}
+export const getSum = (...rest) => {
+  let sum = 0
+  for (let i of rest) {
+    sum += i
+  }
+  return sum
+}
 
 // INPUT: an unknown number of arguments
 // OUTPUT: an array with the first two arguments destructured and the remaining in a nested array
 // REQS: use rest parameters
 // getFirstTwoArgs(1, 2, 3, 4, 5) should return [1, 2, [3, 4, 5]]
 // getFirstTwoArgs('a', 'b', 'c', 'd') should return ['a', 'b', ['c', 'd']]
-export const getFirstTwoArgs = () => {}
+export const getFirstTwoArgs = (a, b, ...rest) => {
+  return [a, b, rest]
+}
 
 // INPUT: an object with the following structure
 // {
@@ -70,16 +78,28 @@ export const getFirstTwoArgs = () => {}
 //    return a NEW object, do not modify the object passed in to the function
 //    use spread operator to create a new object
 
-export const addSneakerCount = () => {}
+export const addSneakerCount = ({ shoes, slogan, logo, headquarters }) => {
+  const shoeList = [...shoes]
+  const sneakerCount = shoeList.length
+  return { shoes, slogan, logo, headquarters, sneakerCount }
+}
 
 // INPUT: brands from data.js
 // OUTPUT: the brand names listed
 // REQS: use Object.keys to solve
-export const getBrandNames = () => {}
+export const getBrandNames = (brands) => {
+  return Object.keys(brands)
+}
 
 // INPUT: brands from data.js
 // OUTPUT: total number of sneaker types across all brands (14)
-export const totalSneakerCount = () => {}
+export const totalSneakerCount = (brands) => {
+  const sneakers = []
+  for (const brand of Object.keys(brands)) {
+    sneakers.push(...brands[brand].shoes)
+  }
+  return sneakers.length
+}
 
 // INPUT: An object
 // OUTPUT: An array with key value pairs converted to arrays
@@ -88,6 +108,15 @@ export const totalSneakerCount = () => {}
 // convertToArray({}) => []
 // Source: https://edabit.com/challenge/pPNAs5PvB3WvnDwDM
 
-export const convertToArray = () => {}
+// export const convertToArray = (obj) => {
+//   const arr = []
+//   for (const key of Object.keys(obj)) {
+//     arr.push([key, obj[key]])
+//   }
+//   return arr
+// }
+export const convertToArray = (obj) => {
+  return Object.entries(obj)
+}
 
 //
